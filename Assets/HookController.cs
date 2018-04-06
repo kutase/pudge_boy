603393b8ecff6c00df8e51bdeb792390aebbb65b
Kutase <kutase123@gmail.com>
2018-04-07 02:08:55 +0500

    init

---
+++ Assets/HookController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HookController : MonoBehaviour {
+
+    private SliderJoint2D distanceJoint;
+    private int terrainLayer = -1;
+
+	// Use this for initialization
+	void Start () {
+        distanceJoint = GetComponent<SliderJoint2D>();
+        terrainLayer = LayerMask.GetMask("Terrain");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!distanceJoint.enabled)
+            {
+                Vector3 pointerPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                pointerPosition.z = 0;
+                RaycastHit2D hit = Physics2D.Raycast(transform.position, pointerPosition - transform.position, 100f, terrainLayer);
+
+                Debug.DrawRay(transform.position, pointerPosition - transform.position, Color.green);
+
+                if (hit.collider != null)
+                {
+                    hit.point = new Vector2(hit.point.x, hit.point.y);
+
+                    distanceJoint.connectedAnchor = hit.point;
+                    distanceJoint.enabled = true;
+                }
+            }
+        } else if (Input.GetMouseButtonUp(0))
+        {
+            distanceJoint.enabled = false;
+        }
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Animator animator;
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        var xAxis = Input.GetAxis("Horizontal");
+        var velocity = rb.velocity;
+
+        if (xAxis != 0)
+        {
+            animator.SetBool("isMoving", true);
+            velocity.x = 10f * xAxis;
+            sr.flipX = xAxis < 0;
+        } else
+        {
+            animator.SetBool("isMoving", false);
+            velocity.x = 0f;
+        }
+
+        rb.velocity = velocity;
+    }
+}
